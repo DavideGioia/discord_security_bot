f5c89f424e79f5da2bee959d1067c5ea515939f2
DavideGioia <giastru@DESKTOP-H4FTGGT.localdomain>
2022-05-27 02:35:29 +0200

    add a config file

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const { Client, Intents } = require("discord.js");
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
+const config = require("./config.json");
 
-// Set the prefix
-let prefix = "!";
 client.on("messageCreate", (message) => {
     // Exit and stop if the prefix is not there or if user is a bot
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    if (message.content.startsWith(`${prefix}ping`)) {
+    if (message.content.startsWith(`${config.prefix}ping`)) {
         message.channel.send("pong!");
     } else
 
-        if (message.content.startsWith(`${prefix}foo`)) {
+        if (message.content.startsWith(`${config.prefix}foo`)) {
             message.channel.send("bar!");
         }
 });
 
-client.login("OTc5NTI0MTY5NDg4MjkzODg4.GvJbry.GSjrwpcERZmf-HJv6DPxWjL3DIFvSbw1LhDKhY");
+client.login(config.token);
